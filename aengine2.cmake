3b7278c465898d80d4f72a22f12feecdaeae3dae
klokik <0xb000@gmail.com>
2013-06-08 21:34:40 +0300

    add build system

---
+++ aengine2.cmake
@@ -0,0 +1,11 @@
+set("${PROJECT}_VERSION_MAJOR" 2)
+set("${PROJECT}_VERSION_MAJOR" 0)
+
+add_definitions(-std=c++11)
+
+set(CMAKE_BUILD_TYPE Debug)
+
+add_definitions(-Wall)
+# set(CMAKE_CXX_FLAGS_DEBUG ${CMAKE_CXX_FLAGS_DEBUG} "-rdynamic")
+
+#set configuration header file for source code
---
+++ includes.cmake
@@ -0,0 +1,10 @@
+set(INCLUDES ${INCLUDES}
+	Base/include
+	RenderUnits/GL/include
+	RenderUnits/GLSL/include
+	SysDependent/include
+	Plugins/AEVision/include
+	Plugins/COLLADA/include
+	/usr/include/collada-dom2.4/
+	# /usr/include/collada-dom2.4/1.4/
+	/usr/include/collada-dom2.4/1.5/)
