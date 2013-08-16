1e4520d3c6e1870cec594c5617fdeb8bbb9722f5
klokik <0xb000@gmail.com>
2013-08-16 11:35:56 +0300

    builtin shaders

---
+++ resource/shader/builtin.h
@@ -0,0 +1,45 @@
+#ifndef BUILTIN_H_
+#define BUILTIN_H_
+
+#include <stddef.h>
+
+
+const char *_3vc_v=
+{
+"#version 100\n"
+"\n"
+"uniform mat4 u_object_matrix;\n"
+"uniform mat4 u_camera_matrix;\n"
+"uniform mat4 u_projection_matrix;\n"
+"\n"
+"uniform vec4 u_color;\n"
+"\n"
+"attribute vec3 a_vertex;\n"
+"attribute vec4 a_color;\n"
+"\n"
+"varying vec4 f_color;\n"
+"\n"
+"void main(void)\n"
+"{\n"
+"	vec4 pos=u_object_matrix*vec4(a_vertex,1.0);\n"
+"	gl_Position=u_projection_matrix*u_camera_matrix*pos;\n"
+"\n"
+"	f_color=a_color*u_color;\n"
+"}\n"
+""
+};
+
+const char *_3vc_f=
+{
+"#version 100\n"
+"\n"
+"varying vec4 f_color;\n"
+"\n"
+"void main(void)\n"
+"{\n"
+"	gl_FragData[0] = f_color;\n"
+"}\n"
+""
+};
+
+#endif
