9ac610a7ee87b3bacb30778a50baebf6835bc50f
klokik <0xb000@gmail.com>
2013-08-16 11:21:59 +0300

    switchable GLES version

--- RenderUnits/GL/include/AEGLHeader.h
+++ RenderUnits/GL/include/AEGLHeader.h
@@ -3,16 +3,17 @@
 
 #include "render_build_config.h"
 
-#if defined(BUILD_GLES_RENDER)
-
-#include <GLES2/gl2.h>
-#define AE_NEW_GL_CONTEXT
-
-#elif defined(BUILD_GL_RENDER)||defined(BUILD_GLSL_RENDER)
-
 #ifndef GL_GLEXT_PROTOTYPES
 #define GL_GLEXT_PROTOTYPES
 #endif
+
+#if defined(BUILD_GLES_RENDER)
+
+//#include <GLES2/gl2.h>
+#include <GLES3/gl3.h>
+#define AE_NEW_GL_CONTEXT
+
+#elif defined(BUILD_GL_RENDER)||defined(BUILD_GLSL_RENDER)
 
 #include <GL/gl.h>
 
