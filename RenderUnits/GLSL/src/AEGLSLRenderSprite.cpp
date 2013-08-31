b67588c5c767edef2afed53671d782a8d5707162
klokik <0xb000@gmail.com>
2013-08-31 08:59:52 +0300

    reimplemented perspective sprite rendering

--- RenderUnits/GLSL/src/AEGLSLRenderSprite.cpp
+++ RenderUnits/GLSL/src/AEGLSLRenderSprite.cpp
@@ -7,6 +7,8 @@
 
 #include "AEGLHeader.h"
 #include "AEGLSLRender.h"
+#include "AEVectorMath.h"
+#include "AEDebug.h"
 
 
 namespace aengine
@@ -16,22 +18,39 @@ namespace aengine
 		glBindFramebuffer(GL_FRAMEBUFFER,fbo_onds);
 
 		glDisable(GL_DEPTH_TEST);
-		glDisable(GL_STENCIL_TEST);
-		glDisable(GL_BLEND);
 
 		p_3vm->Use();
 
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,sprite_mesh.idfce);
 
+		AEMatrix4f4 mtx;
 		for(AEObjectSprite *obj:type_cache.sprites_persp)
 		{
+			//TODO: Far not the best solution
+			// <hell>
+			AEMatrix4f4 A=cammatrix*obj->GetWorldMatrix();
+			Vec3f scale= obj->GetAbsScale();
+			A[0]=scale.X;
+			A[5]=scale.Y;
+			A[10]=scale.Z;
+			A[15]=1.0f;
+			float mtx_f[]={
+				A[ 0],    0,    0,    0,
+				    0,A[ 5],    0,    0,
+				    0,    0,A[10],    0,
+				A[12],A[13],A[14],A[15]
+			};
+
+			mtx=mtx_f;
+
 			p_3vm->BindData(
-				obj->GetWorldMatrix(),
-				cammatrix,
+				mtx,
+				identity,
 				prjmatrix,
 				&this->sprite_mesh,
 				obj->material
 			);
+			// </hell>
 
 			glDrawElements(GL_TRIANGLES,2*3,GL_UNSIGNED_INT,NULL);
 		}
@@ -48,7 +67,6 @@ namespace aengine
 
 		glDisable(GL_DEPTH_TEST);
 		glDisable(GL_STENCIL_TEST);
-		glDisable(GL_BLEND);
 
 		p_3vm->Use();
 
