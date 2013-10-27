f347c24d5c276548a04abd74e2badeb9b6057ca3
klokik <0xb000@gmail.com>
2013-10-27 11:27:55 +0200

    add ES version of shaders

---
+++ resource/shader/deferred/mesh_f_es.glsl
@@ -0,0 +1,23 @@
+#version 100
+
+precision highp float;
+uniform vec4 u_color;
+
+uniform sampler2D u_texture0;
+uniform int u_istextured0;
+
+varying vec3 f_normal;
+varying vec2 f_texcoord0;
+varying vec3 f_pos;
+
+void main(void)
+{
+	gl_FragData[0]=u_color;
+	if(u_istextured0!=0)
+	{
+		gl_FragData[0]*=texture2D(u_texture0,f_texcoord0.st);
+	}
+
+	// gl_FragData[1] = vec4(f_pos.x,f_pos.y,-f_pos.z,gl_FragCoord.z);
+	// gl_FragData[2] = vec4(f_normal,1.0);
+}
